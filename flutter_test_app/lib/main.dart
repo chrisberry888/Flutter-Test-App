58c78816bafbced3b448e39f7a5e8c7464cb980e
Chris Berry <chrisberry888@gmail.com>
2023-05-16 13:50:22 -0400

    Added scrollable widget generation

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -8,11 +8,15 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+
   int _count = 0;
   List<Row> _rows = [];
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -25,13 +29,13 @@ class _MyAppState extends State<MyApp> {
 //        body: Column(
         //         children: _rows.map((row) => Row(children: row)).toList(),
 //        ),
-        body: Center(
-          child: Container(
-              child: Column(
-                  children: _rows
-              ),
-            margin: const EdgeInsets.all(0),
-            padding: const EdgeInsets.all(0),
+        body: Container(
+          margin: const EdgeInsets.all(0),
+          padding: const EdgeInsets.all(10),
+          child: SingleChildScrollView(
+            child: Column(
+                children: _rows
+            ),
           ),
         ),
 
@@ -50,17 +54,23 @@ class _MyAppState extends State<MyApp> {
       if (_count % colnum == 0) {
         _rows.add(Row(
           children: [
-            _buildRectangle(),
+            _buildRectangle(_count),
           ],
         ));
       } else if (_count % colnum == 1){
-        _rows[_count ~/ colnum] = Row(
+        _rows[_count - 1 ] = Row(
           children: [
-            ..._rows[_count ~/ colnum].children,
+            ..._rows[_count - 1].children,
             Padding(padding: EdgeInsets.all(10)),
-            _buildRectangle(),
+            _buildRectangle(_count),
           ],
         );
+
+        _rows.add(Row(
+          children: [
+            _buildSpacer(),
+          ],
+        ));
       }
 
       _count += 1;
@@ -69,12 +79,29 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Widget _buildSpacer() {
+    return Container(
+      width: 1,
+      height: 10,
+      color: Colors.transparent,
+    );
+  }
 
-  Widget _buildRectangle() {
+  Widget _buildRectangle(int num) {
     return Container(
       width: 150,
       height: 100,
       color: Colors.red,
+      child: Center(
+        child: Text(
+          (num+1).toString(),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          ),
+        ),
+      ),
+
     );
   }
 
